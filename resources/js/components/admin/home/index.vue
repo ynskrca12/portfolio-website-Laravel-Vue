48adaa5058371346eff0bfc702e602b00ec077a3
ynskrca12 <ynskrca52@outlook.com>
2023-01-27 23:49:53 +0300

    Login And Register Sanctum Auth With Vuejs 3

--- resources/js/components/admin/home/index.vue
+++ resources/js/components/admin/home/index.vue
@@ -1,6 +1,19 @@
+<script>
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+const logout = () => {
+    localStorage.removeItem('token')
+    router.push('/')
+}
+</script>
 <template>
     <h1>welcome to admin overiview dashboard</h1>
     <div>
         <router-link to="/">Home page</router-link>
     </div>
+    <br>
+    <button @click="logout">
+        Log out
+    </button>
 </template>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter,createWebHistory } from "vue-router";
 //admin
-
 import homeAdminIndex from '../components/admin/home/index.vue'
 //pages
 import homePageIndex from '../components/pages/home/index.vue'
+//login
+import login from '../components/auth/login.vue'
 
 import notFound from '../components/notFound.vue'
 
@@ -11,25 +12,57 @@ const routes = [
     //admin
     {
         path: '/admin/home',
-        component: homeAdminIndex
+        name:'adminHome',
+        component: homeAdminIndex,
+        meta:{
+            requiresAuth:true
+        }
     },
 
     //Pages
     {
         path: '/',
-        component: homePageIndex
+        name:'Home',
+        component: homePageIndex,
+        meta:{
+            requiresAuth:false
+        }
+    },
+
+    //Login
+    {
+       path:'/login',
+       name:'Login',
+       component:login,
+       meta:{
+        requiresAuth:false
+    }
     },
 
     //notFound
     {
-        path: '/:pathMatch(.*)',
-        component: notFound
+        path: '/:pathMatch(.*)*',
+        name:'notFound',
+        component: notFound,
+        meta:{
+            requiresAuth:false
+        }
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.beforeEach((to,from) =>{
+    if(to.meta.requiresAuth && !localStorage.getItem('token')){
+        return {name:'Login'}
+    }
+
+    if(to.meta.requiresAuth == false && localStorage.getItem('token')){
+        return {name:'adminHome'}
+    }
 })
 
 export default router
